688d8ed8411c51039c7af6ca8e69b4ce580a1d55
Pablo Sanchidrian <pablosanchidrian@wlanipr24143.uca.es>
2022-11-03 11:22:06 +0100

    gui

---
+++ gui.py
@@ -0,0 +1,50 @@
+import streamlit as st
+
+from streamlit_ace import st_ace
+
+# Spawn a new Ace editor
+# content = st_ace()
+
+# Display editor's content as you type
+# content
+# st.write('dio')
+
+placeholder = '''#include <stdio.h>\n\n// your code here...'''
+with st.sidebar:
+    st.header('C to Assembly')
+    choice = st.radio("Choose input method", ("Online Editor", "Pick a file"))
+    
+if choice == 'Online Editor':
+    content = st_ace(language='c_cpp', theme='dracula',
+                     value=placeholder, keybinding='vscode',
+                     tab_size=4,)
+          
+    filename = st.text_input(label='', placeholder='Enter filename')
+        
+    if filename is not None:
+    
+        if st.button('Save file'):
+            if filename != '':
+                with open(f'{filename}.c', 'w') as file:
+                    file.write(content)  
+        
+                
+                st.download_button(label='Download .C', data=content,file_name=f'{filename}.c')
+    
+    if st.button('Run'):
+        st.write('running...')
+        
+     
+if choice == 'Pick a file':
+    uploaded_file = st.file_uploader("Choose a file")
+    
+    if uploaded_file is not None:
+        print(uploaded_file)
+        with open(uploaded_file.name, 'r') as file:
+            executable = file.read()
+    
+        content2 = st_ace(language='c_cpp', theme='dracula',
+                     value=executable, keybinding='vscode',
+                     tab_size=4,)
+    
+        
